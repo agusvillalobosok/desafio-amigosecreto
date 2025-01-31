97500452704dd76477d095026596f167bea608a5
agusvillalobosok <agustindaker@gmail.com>
2025-01-31 17:59:54 -0300

    Update app.js
    Mejore el código agregando una función para actualizar la lista de forma independiente a la función (agregarAmigos) que agregaba los nombres.  También se agregó validaciones para evitar nombres repetidos o espacios en blanco.

--- app.js
+++ app.js
@@ -2,34 +2,64 @@
 
 
 
-
+// nombres = array principal, está vacía pero recibe los datos del input en html.
 const nombres = [] ;
 
+// inputAmigos = un puntero o variable que enlaza el elemento (input) donde se escriben los nombres para utilizarlo en 
+//               la funcion.
 const inputAmigos = document.getElementById ("amigo"); 
 
+
+// listaAmigos = un puntero que enlaza el elemento lista que se encuentra vacio, para recibir luego con la funcion los nombres.
 const listaAmigos = document.getElementById ("listaAmigos") ; 
 
-const resultado = document.getElementById ("resultado") ; 
+// resultado = un puntero que enlaza el elemento lista que se encuentra vacio. que recibe el nombre sorteado por medio 
+//             de la funcion sortearAmigo. 
+const resultado = document.getElementById ("resultado") ;
+
+
 
 function agregarAmigo () {
-  if (inputAmigos.value != "") {
-    nombres.push(inputAmigos.value);
+
+          let nombre = inputAmigos.value.trim() ;
+  
+          inputAmigos.value = "" ; 
+  
+          if (nombre == "") {
+          alert("Por favor ingresar nombres validos.")
+            return;
   } 
-
- const li = document.createElement ("li") ;
-
-  li.textContent = inputAmigos.value ; 
-
-  listaAmigos.appendChild(li); 
-
-  inputAmigos.value = "" ; 
   
+    if ( nombres.includes(nombre)) {
+        alert("Ese nombre ya está en la lista, por favor ingresar otro.")
+      return;
+    }
+
+    nombres.push(nombre); 
+
+  actualizarLista() ; 
+ 
+}
+
+
+
+function actualizarLista () {
+  listaAmigos.innerHTML = "" ; 
+
+  for (let i = 0; i< nombres.length; i++) {
   
+  const li = document.createElement ("li") ;
+
+  li.textContent = nombres[i] ; 
+
+  listaAmigos.appendChild(li);   
+    
+  }
 }
 
 function sortearAmigo () {
       if (nombres.length <= 1 ) {
-        alert("No hay nombres suficientes, no se puede sortear"); 
+        alert("No hay nombres suficientes, no se puede sortear."); 
         return; 
       } 
         const indiceAleatorio = Math.floor(Math.random() *nombres.length); 
